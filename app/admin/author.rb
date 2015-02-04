fe9cc7071d86eb54e5d7f91fd77b2501f0078aba
Alexandre Monjol <alexandre.monjol@gmail.com>
2015-02-04 18:02:19 +0100

    Export/Importt CSV files

--- app/admin/author.rb
+++ app/admin/author.rb
@@ -5,6 +5,11 @@ ActiveAdmin.register Author do
       t.column("Title") { |book| book.name }
     end
   end
+  # Formate la sortie du ficher CSV pour sortir que les colone first name et lastname
+  csv do
+    column :first_name
+    column :last_name
+  end
 
   active_admin_importable
 
